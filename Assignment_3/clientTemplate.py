4e5f2071e0758aa92326d589eab363777b15b3b9
Ibrahim Siddig <38038497+ibrahimssd@users.noreply.github.com>
2021-05-07 03:40:11 +0200

    Add files via upload

--- Assignment_3/clientTemplate.py
+++ Assignment_3/clientTemplate.py
@@ -3,28 +3,34 @@
 #
 from socket import *
 import hashlib
-from os import path,listdir
+from os import path, listdir
+
+
 #
 # Generate md5 hash function
 #
-def generate_md5_hash (file_data):
+def generate_md5_hash(file_data):
     md5_hash = hashlib.md5(file_data)
     f_id = md5_hash.hexdigest()
     return str(f_id)
-# 
+
+
+#
 # Define Server URL and PORT
 #
 serverPort = 7700
 serverURL = "localhost"
-# 
+#
 # Create TCP socket for future connections
 #
 clientSocket = socket(AF_INET, SOCK_STREAM)
-# 
+#
 # Connect the client to the specified server
 #
 clientSocket.connect((serverURL, serverPort))
 print("Client connected to server: " + serverURL + ":" + str(serverPort))
+
+
 #
 # This client implements the following scenario:
 # 1. LIST_FILES
@@ -34,11 +40,11 @@ print("Client connected to server: " + serverURL + ":" + str(serverPort))
 # 4a. DOWNLOAD the previously uploaded file
 # 4b. Check MD5
 #
-#close TCP connection
+# close TCP connection
 
 def list_files():
     request = 'LIST_FILES'
-    print("Listing Server Files")
+    print("Listing Files Available on the Server : ")
     clientSocket.send(request.encode())
     responsedict = {}
     while True:
@@ -50,23 +56,28 @@ def list_files():
             responsedict[splitresponse[0]] = tuple(splitresponse[1:])
             continue
         break
-    print("Listing files complete")
+
+    print("Listing files completed")
+
     return responsedict
 
 
-def download(file_id,file_name,file_size):
+def download():
     request = 'DOWNLOAD'
-    print("Downloading fileid:{}, file_name:{}, file_size:{})".format(file_id,file_name,file_size))
     clientSocket.send(request.encode())
     response = clientSocket.recv(1024).decode()
-    print("server:"+response)
+    print("server:" + response)
+    file_id = input("Please Enter The File ID:")
+    file_name = files_data[file_id][0]
+    file_size = int(files_data[file_id][1])
+    print("Downloading file_id:{}, file_name:{}, file_size:{})".format(file_id, file_name, file_size))
     request = file_id
-    with open(".\client\\"+file_name,'wb') as downloadedfile:
+    with open(".\client\\" + file_name, 'wb') as downloadedfile:
         clientSocket.send(request.encode())
-        endi = int(file_size)//1024
+        endi = int(file_size) // 1024
         i = 0
         while response and i < endi:
-            i+=1
+            i += 1
             response = clientSocket.recv(1024)
             if response:
                 try:
@@ -75,61 +86,65 @@ def download(file_id,file_name,file_size):
                     print(e)
                 continue
             break
-    md5 = generate_md5_hash(open(".\client\\"+file_name,'rb').read())
-    print("MD5:",md5)
+    md5 = generate_md5_hash(open(".\client\\" + file_name, 'rb').read())
+    print("client MD5:", md5)
     if md5 == file_id:
-        print("Download sucess")
+        print("Hashes Matching '\u2713' :Download Success '\u2713' ")
     else:
         print("Download fail: MD5 check failure")
-        
-def upload(file_name):
+
+
+def upload():
     request = 'UPLOAD'
     clientSocket.send(request.encode())
     response = clientSocket.recv(1024).decode()
-    print("server:"+response)
-    request = "{};{}".format(file_name, path.getsize(".\client\\"+file_name))
+    print("server:" + response)
+    file_name = input("File name:")
+    file_size = input("File size:")
+
+    # request = "{};{}".format(file_name, path.getsize(".\client\\" + file_name))
+    request = "{};{}".format(file_name, file_size)
     clientSocket.send(request.encode())
     response = clientSocket.recv(1024).decode()
-    with open(".\client\\"+file_name,'rb') as file:
+    print(response)
+    with open(".\client\\" + file_name, 'rb') as file:
         try:
+            print("Start sending.....")
             data = file.read(1024)
             while data:
                 clientSocket.send(data)
                 data = file.read(1024)
         except:
             pass
-    #check hash
+    # check hash
     response = clientSocket.recv(1024).decode()
-    md5 = generate_md5_hash(open(".\client\\"+file_name,'rb').read())
-    print("MD5:",md5)
-    print("response:",response)
+    md5 = generate_md5_hash(open(".\client\\" + file_name, 'rb').read())
+    print("MD5:", md5)
+    print("Hash response from server :", response)
     if md5 == response:
-        print("Upload sucess")
+        print("Hashes Matching '\u2713' : Upload success '\u2713' ")
     else:
         print("Upload fail: MD5 check failure")
-    
+
+
 helpcommands = """1. list files in client directory
 2. LIST_FILES from server
 3. DOWNLOAD a file from the server (call list files first)
 4. UPLOAD a file to a server
 """
 print("type help to get list of commands. Commands are numbers only.")
-files_data = {} #maps ids to (file_name,file_size)
+files_data = {}  # maps ids to (file_name,file_size)
 while True:
     cmd = input("Client>>")
     if cmd == '1':
         print(listdir(".\client"))
     elif cmd == '2':
         files_data = list_files()
-        print("files_data:",files_data)
+        print("files_data:", files_data)
     elif cmd == '3':
-        file_id = input("File ID:")
-        file_name = files_data[file_id][0]
-        file_size = int(files_data[file_id][1])
-        download(file_id, file_name, file_size)
+         download()
     elif cmd == '4':
-        file_name = input("File name:")
-        upload(file_name)
+         upload()
     elif cmd == 'help':
         print(helpcommands)
 
--- Assignment_3/serverTemplate.py
+++ Assignment_3/serverTemplate.py
@@ -4,52 +4,57 @@
 from socket import *
 import hashlib
 from os import listdir, path
+
+
 #
 # Generate md5 hash function
 #
-def generate_md5_hash (file_data):
+def generate_md5_hash(file_data):
     md5_hash = hashlib.md5(file_data)
     f_id = md5_hash.hexdigest()
     return str(f_id)
-# 
+
+
+#
 # Define Server URL and PORT
 #
 serverPort = 7700
 serverURL = "localhost"
-# 
+#
 # Create TCP socket for future connections
 #
 serverSocket = socket(AF_INET, SOCK_STREAM)
-# 
+#
 # Bind URL and Port to the created socket
 #
 serverSocket.bind((serverURL, serverPort))
-# 
+#
 # Start listening for incoming connection (1 client at a time)
 #
 serverSocket.listen(1)
 print("Server is listening on port: " + str(serverPort))
 
-file_ids_to_names = {} #used in download and upload
-file_names_to_ids = {} #used in list_files
+file_ids_to_names = {}  # used in download and upload
+file_names_to_ids = {}  # used in list_files
 
 for f in listdir('.\server'):
-    file_id = generate_md5_hash(open(".\server\\"+f,'rb').read()) #this is just temporary
+    file_id = generate_md5_hash(open(".\server\\" + f, 'rb').read())  # this is just temporary
     file_ids_to_names[file_id] = f
     file_names_to_ids[f] = file_id
 
 while True:
-    # 
+    #
     # Accept incoming client connection
     #
     connectSocket, addr = serverSocket.accept()
     print("Client connected: " + str(addr))
     while True:
         request = connectSocket.recv(1024).decode()
-        if request=='LIST_FILES':
+        if request == 'LIST_FILES':
             print("LIST_FILES command received")
             print("Listing files..")
-            filelist = [ "{};{};{}".format(file_names_to_ids[f],f, path.getsize(f)) for i,f in enumerate(listdir('.\server'))]
+            filelist = ["{};{};{}".format(file_names_to_ids[f], f, path.getsize(f)) for i, f in
+                        enumerate(listdir('.\server'))]
             if filelist:
                 response = '\n'.join(filelist)
                 print("Listing files")
@@ -66,7 +71,7 @@ while True:
             file_id = connectSocket.recv(1024).decode()
             print("OK: file id received :{}".format(file_id))
             if file_id in file_ids_to_names:
-                with open(".\server\\"+file_ids_to_names[file_id],'rb') as file:
+                with open(".\server\\" + file_ids_to_names[file_id], 'rb') as file:
                     print("file opened: {}".format(file_ids_to_names[file_id]))
                     print("Sending file..")
                     try:
@@ -77,29 +82,29 @@ while True:
                         print("OK: file transfer finished")
                     except:
                         pass
-            #Add handling for when file id does not exist\
+            # Add handling for when file id does not exist\
 
         elif request == 'UPLOAD':
             print("UPLOAD command received from client")
             response = "OK: Waiting for file data \"file_name;file_size\"."
             print(response)
             connectSocket.send(response.encode())
-            file_name,file_size = connectSocket.recv(1024).decode().split(';')
-            with open(".\server\\"+file_name,'wb') as file:
+            file_name, file_size = connectSocket.recv(1024).decode().split(';')
+            with open(".\server\\" + file_name, 'wb') as file:
                 response = "OK: Ready to receive file"
                 print(response)
                 connectSocket.send(response.encode())
-                endi = int(file_size)//1024
+                endi = int(file_size) // 1024
                 i = 0
                 while True and i < endi:
-                    i+=1
+                    i += 1
                     response = connectSocket.recv(1024)
                     if response:
                         file.write(response)
                         continue
                     break
             print("OK: file send complete")
-            file_id = generate_md5_hash(open(".\server\\"+f,'rb').read())
+            file_id = generate_md5_hash(open(".\server\\" + f, 'rb').read())
             connectSocket.send(file_id.encode())
             print("OK: md5 hash sent")
             if file_name in file_names_to_ids:
@@ -108,6 +113,6 @@ while True:
                 del file_ids_to_names[tempid]
             file_ids_to_names[file_id] = file_name
             file_names_to_ids[file_name] = file_id
-            
-    #close TCP connection
+
+    # close TCP connection
     connectSocket.close()
